073cc07ef6bdb27a2150de8118f5b8e025567023
arildbo <arthurarild.bo@benilde.edu.ph>
2021-02-09 11:47:06 +0800

    Revert "Added Articles"
    This reverts commit 9036789405f60f2053df4678ece1c15650e75ea0.

--- routes/web.php
+++ routes/web.php
@@ -76,8 +76,3 @@ Route::delete('/admin/plantreference/{id}/delete', 'PlantReferencepageController
 //     return 'upluad success';
 // });
 
-
-Route::get('admin/categories', 'CategorieController@index')/*->middleware('admin')*/;
-
-Route::resource('articles', 'ArticleController');
-
