22a943e6df71d67aa54929df4cfe283fa1cd8e53
Kenny <47163761+Gaaame@users.noreply.github.com>
2021-02-11 00:49:30 +0800

    updated create article and view article

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,47 +1,63 @@
-@extends('articles.layout')
+@extends('layouts.template')
   
 @section('content')
+
+<div class="container ">
+    <div class="row px-4 mx-auto">
+        <div class="col-lg-10 col-xl-10 card flex-row mx-auto px-4 shadow p-3 mb-5 border-3">
+            <div class="card-body mx-auto">
+                <div >
+                    <h2 class="card-text text-center">Create New Article</h2>
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <strong>Warning!</strong> Please check your input code<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                   
+                <form action="{{ route('articles.store') }}" method="POST">
+                    @csrf
+                     <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Title:</strong>
+                                <input type="text" name="title" class="form-control" placeholder="Title" id="title">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Description:</strong>
+                                <textarea class="form-control" style="height:280px" name="description" placeholder="Description" id="description"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button type="submit" class="btn btn-block btn-success text-uppercase text-white">Submit</button>
+                                <a class="btn btn-block btn-primary mt-2" href="{{ route('articles.index') }}"> Back</a>
+                        </div>
+                    </div>
+                   
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Create New Article</h2>
+          
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('articles.index') }}"> Back</a>
+           
         </div>
     </div>
 </div>
    
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Warning!</strong> Please check your input code<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-   
-<form action="{{ route('articles.store') }}" method="POST">
-    @csrf
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                <input type="text" name="title" class="form-control" placeholder="Title" id="title">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                <textarea class="form-control" style="height:280px" name="description" placeholder="Description" id="description"></textarea>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
-   
-</form>
+
 @endsection
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,51 +1,70 @@
-@extends('articles.layout')
+@extends('layouts.template')
 @section('content')
 
 <div class="row">
-        <div class="col-lg-12 margin-tb">
+        <div class="col-lg-10 mr-auto ml-auto">
             <div class="pull-left">
-                <h2>Check all Articles</h2>
+             
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('articles.create') }}"> Create new article</a>
             </div>
+        </div>     
+        <div class="container ">
+            <h2>Check all Articles</h2>
+            <div class="card-body table-responsive-sm">
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+           
+            <table class="table table-bordered table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th width="250px">Action</th>
+                    </tr>
+                </thead>
+              
+                @foreach ($articles as $article)
+                <tr>
+                    <td>{{ $article->article_id }}</td>
+                    <td>{{ $article->article_topic }}</td>
+                    <td>{{ $article->article_description }}</td>
+                    <div >
+                        <td class="float-right">
+                            <form class="col-sm-3 my-auto float-right" action="{{ route('articles.destroy',$article->article_id) }}" method="POST" >
+                                @csrf
+                                @method('DELETE')
+                                
+                             
+
+                            </form>
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                            <a class="btn btn-info" href="{{ route('articles.show',$article->article_id) }}">Show</a>
+                
+                            <a class="btn btn-warning" href="{{ route('articles.edit',$article->article_id) }}">Edit</a>
+                        </td>
+                    </div>
+                  
+                </tr>
+                @endforeach
+            </table>
+          
+            {!! $articles->links() !!}
+            </div>
         </div>
     </div>
    
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
    
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th width="250px">Action</th>
-        </tr>
-        @foreach ($articles as $article)
-        <tr>
-            <td>{{ $article->article_id }}</td>
-            <td>{{ $article->article_topic }}</td>
-            <td>{{ $article->article_description }}</td>
-            <td>
-                <form action="{{ route('articles.destroy',$article->article_id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-   
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
 
-                <a class="btn btn-info" href="{{ route('articles.show',$article->article_id) }}">Show</a>
-    
-                <a class="btn btn-primary" href="{{ route('articles.edit',$article->article_id) }}">Edit</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-  
-    {!! $articles->links() !!}
+
+
+
+
+
       
 @endsection
