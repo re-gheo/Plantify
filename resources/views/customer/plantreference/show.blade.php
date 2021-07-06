3bf9cba793b09ca01aaf1e352511414c23f82fda
GhostBoxx <77956979+GhostBoxx@users.noreply.github.com>
2021-07-06 21:49:39 +0800

    Designed Specific Plants Reference Page & Specific Products Page

--- resources/views/customer/plantreference/show.blade.php
+++ resources/views/customer/plantreference/show.blade.php
@@ -1,42 +1,45 @@
 @extends('layouts.template')
 
 @section('content')
-    {{ dump($plant_referencepage) }}
 
-    <h1>
-        SHOW
-    </h1>
+    <br>
+<div class="container card">
+    <br>
+    <div class="text-center">
+        <h1><strong>{{ $plant_referencepage->plant_scientificname }}</strong></h1>
+    </div>
+    <hr>
 
-    <div>
-        image HERE
+    <div class="text-center card">
+        Image supposed to appear here..
         <img src="{{ $plant_referencepage->plant_photo }}" alt="">
         <img src="{{ $plant_referencepage->plant_phototwo }}" alt="">
         <img src="{{ $plant_referencepage->plant_photothree }}" alt="">
     </div>
-    <div>
-        <label>plant_scientificname</label>
-        <p>{{ $plant_referencepage->plant_scientificname }}</p>
+
+    <br>
+
+    <div class="text-center">
+        <label><b>What is the {{ $plant_referencepage->plant_scientificname }}?</b></label>
+        <p class="text-left">{{ $plant_referencepage->plant_description }}</p>
     </div>
 
-    <div>
-        <label>plant_description</label>
-        <p>{{ $plant_referencepage->plant_description }}</p>
+    <div class="text-center">
+        <label><b>How can you maintain the {{ $plant_referencepage->plant_scientificname }}?</b></label>
+        <p class="text-left">{{ $plant_referencepage->plant_maintenance }}</p>
     </div>
 
-    <div>
-        <label>plant_maintenance</label>
-        <p>{{ $plant_referencepage->plant_maintenance }}</p>
+    <br>
+    
+    <div class="text-center">
+        <button class="btn btn-success btn-sm">
+            <a class="text-white" href="{{ route('plant-information.index') }}">Return to References</a>
+        </button>
     </div>
 
-    <div>
-        <label></label>
-        <p></p>
-    </div>
+    <br>
 
-    <div>
-        <label></label>
-        <p></p>
-    </div>
+</div>
 
 
 
