7bcc8f08fa89a57cee422c3be708f16d197a6921
GhostBoxx <77956979+GhostBoxx@users.noreply.github.com>
2021-07-06 22:11:12 +0800

    Revert "idk what this is"
    This reverts commit c5873a9ab7e0d57eb13a87b7262743a724e070f1.

--- resources/views/customer/plantreference/show.blade.php
+++ resources/views/customer/plantreference/show.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.template')
 
 @section('content')
-    {{ dump($plant_referencepage) }}
 
-    <h1>
-        SHOW
-    </h1>
+    <br>
+<div class="container card">
+    <br>
+    <div class="text-center">
+        <h1><strong>{{ $plant_referencepage->plant_scientificname }}</strong></h1>
+    </div>
+    <hr>
 
-<<<<<<< HEAD
     <div class="text-center card">
         Image supposed to appear here..
         <img src="{{asset('/storage/'. $plant_referencepage->plant_photo)  }}" width="300" height="300" alt="main">
@@ -20,47 +22,23 @@
     <div class="text-center">
         <label><b>What is the {{ $plant_referencepage->plant_scientificname }}?</b></label>
         <p class="text-left">{{ $plant_referencepage->plant_description }}</p>
-=======
-    <div>
-        image HERE
-        <img src="{{ $plant_referencepage->plant_photo }}" alt="">
-        <img src="{{ $plant_referencepage->plant_phototwo }}" alt="">
-        <img src="{{ $plant_referencepage->plant_photothree }}" alt="">
-    </div>
-    <div>
-        <label>plant_scientificname</label>
-        <p>{{ $plant_referencepage->plant_scientificname }}</p>
-    </div>
->>>>>>> parent of 3bf9cba (Designed Specific Plants Reference Page & Specific Products Page)
 
-    <div>
-        <label>plant_description</label>
-        <p>{{ $plant_referencepage->plant_description }}</p>
+    <div class="text-center">
+        <label><b>How can you maintain the {{ $plant_referencepage->plant_scientificname }}?</b></label>
+        <p class="text-left">{{ $plant_referencepage->plant_maintenance }}</p>
     </div>
 
-<<<<<<< HEAD
     <br>
 
     <div class="text-center">
         <button class="btn btn-success btn-sm">
             <a class="text-white" href="{{ route('plant-information.index') }}">Return to References</a>
         </button>
-=======
-    <div>
-        <label>plant_maintenance</label>
-        <p>{{ $plant_referencepage->plant_maintenance }}</p>
->>>>>>> parent of 3bf9cba (Designed Specific Plants Reference Page & Specific Products Page)
     </div>
 
-    <div>
-        <label></label>
-        <p></p>
-    </div>
+    <br>
 
-    <div>
-        <label></label>
-        <p></p>
-    </div>
+</div>
 
 
 
